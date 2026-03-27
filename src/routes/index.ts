88e3879f13a0aa9c8e15e7eaf8b1cbf92374e03c
Lukas Manuel Graf <graflukas218@gmail.com>
2026-03-27 17:08:42 +0100

    fix(routes): add i18n /en routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,18 +3,34 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import peakflow from "peakflow";
 import { onWidgetLoad } from "peakflow/elfsight";
 
-export function app() {
-  new WFRoute("/").execute(() => {
-    onWidgetLoad(
-      `[data-elfsight-embed="google-reviews"]`,
-      ScrollTrigger.refresh,
-      {
-        delay: 200,
-        conditions: (widget) =>
-          Boolean(widget.querySelector(".swiper-initialized")),
-      },
-    );
+export interface Routes {
+  [x: string]: string[];
+}
 
-    peakflow.execute("swiper");
-  });
+export const routes: Routes = {
+  home: ["/", "/en"],
+  about: ["/uber-uns", "/en/about"],
+  giftCards: ["/gutscheine", "/en/gift-cards"],
+  events: ["/events", "/en/events"],
+  imprint: ["/impressum", "/en/imprint"],
+  privacy: ["/datenschutz", "/en/privacy"],
+  eventItem: ["/events/(.*)", "/events-en/(.*)"],
+};
+
+export function app() {
+  for (const route of routes.home) {
+    new WFRoute(route).execute(() => {
+      onWidgetLoad(
+        `[data-elfsight-embed="google-reviews"]`,
+        ScrollTrigger.refresh,
+        {
+          delay: 200,
+          conditions: (widget) =>
+            Boolean(widget.querySelector(".swiper-initialized")),
+        },
+      );
+
+      peakflow.execute("swiper");
+    });
+  }
 }
